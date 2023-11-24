e0525805325c69db9d09e588a46251c94a96b992
Kövesdi Tamás <34796547+Kovesditomi@users.noreply.github.com>
2023-11-24 12:36:50 +0100

    Add files via upload

---
+++ Laptopok/Laptopok/Laptop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Laptopok
+{
+    internal class Laptop
+    {
+        public int Azonosito { get; set; }
+        public double ProcTipSeb{ get; set; }
+        public string Oprendszer { get; set; }
+        public string GYartoModell { get; set; }
+        public double AkksiElettartam { get; set; }
+        public double Suly { get; set; }
+        public string VezetekNelkuli { get; set; }
+
+        public Laptop(string r) {
+
+
+        string[] s = r.Split("|");
+            Azonosito = int.Parse(s[0]);
+            ProcTipSeb = double.Parse(s[1]);
+            Oprendszer= s[2];
+            GYartoModell= s[3];
+            AkksiElettartam = double.Parse(s[4]);
+            Suly= double.Parse(s[5]);
+            VezetekNelkuli = s[6];
+
+
+
+
+
+
+        }
+        public override string ToString()
+        {
+            return $"Azonosító:{Azonosito}" +
+                $",Processzor típusa és sebessége:{ProcTipSeb}" +
+                $",Operációs rendszer:{Oprendszer}" +
+                $",Gyártó,és modell:{GYartoModell}," +
+                $"Akkumlátor élettartama:{AkksiElettartam}," +
+                $"Súly:{Suly}," +
+                $"Vezeték nélküli kapcsolatok:{VezetekNelkuli}";
+        }
+    }
+}
---
+++ Laptopok/Laptopok/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+namespace Laptopok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Laptop> laptops= new ();
+
+            using (var sr = new StreamReader(@"..\..\..\src\laptopok.txt", Encoding.UTF8))
+            { 
+
+               _= sr.ReadLine();
+                while(!sr.EndOfStream) laptops.Add(new Laptop(sr.ReadLine()));
+                foreach(Laptop laptop in laptops) Console.WriteLine(laptop);
+        
+            }
+
+
+            Console.WriteLine("11.feladat:");
+
+
+            var fl10 = Bluetooth(laptops);
+            Console.WriteLine("10.feladat");
+            if()
+
+
+
+
+
+            Console.WriteLine($"9. Feladat: Átlagos processzor sebessége:{AtlagProcSeb(laptops)}");
+
+
+
+
+            var fl8 = Uzemido(laptops);
+
+
+            Console.WriteLine("8.feladat");
+
+
+            Console.WriteLine($"Ennek a laptopnak bírja a legkevesebb ideig az akksija:{fl8[0].AkksiElettartam},ez az a modell:{fl8.Count}");
+
+
+
+
+
+            List <Laptop> fl7 =Win2kg(laptops);
+
+
+            Console.WriteLine($"7. feladat");
+
+
+            Console.WriteLine(fl7.Count > 0 ? $"Ennyi{fl7.Count} ilyen laptop van összesen:":"Nem található ilyen fajta laptop!") ;
+
+
+        }
+        static List<Laptop> Win2kg(List<Laptop> k) => k.Where(k => k.Oprendszer == "Windows" &&k.Suly>2).ToList();
+
+
+
+
+        static List<Laptop> Uzemido(List<Laptop> k) => k.Where(k => k.AkksiElettartam <= 6).ToList();
+
+
+        static double AtlagProcSeb(List<Laptop> k) => k.Average(k => k.ProcTipSeb);
+
+
+
+        static List<Laptop> Bluetooth(List<Laptop> k) => k.Where(k => k.VezetekNelkuli == " Bluetooth 5.2"&&k.VezetekNelkuli==k.GYartoModell).ToList();
+
+        static List<int> AMD(List<Laptop> k) => k.Where(k => k.ProcTipSeb.Countains("AMD" && k.Suly > 1, 3)ToList();
+
+    }
+}
